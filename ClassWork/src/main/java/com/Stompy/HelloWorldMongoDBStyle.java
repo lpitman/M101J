4c1c4171a96e22e0ec5db07c73f2a1c55ba2251f
Logan Pitman <pitman.logan@gmail.com>
2014-08-06 17:19:06 -0300

    Added Spark Style HelloWorld

--- ClassWork/src/main/java/com/Stompy/HelloWorldMongoDBStyle.java
+++ ClassWork/src/main/java/com/Stompy/HelloWorldMongoDBStyle.java
@@ -1,10 +1,20 @@
 package com.Stompy;
 
+import com.mongodb.*;
+
+import java.net.UnknownHostException;
+
 /**
  * Created by Logan on 06/08/2014.
  */
 public class HelloWorldMongoDBStyle {
-    public static void main(String[] args){
+    public static void main(String[] args) throws UnknownHostException{
+        MongoClient client = new MongoClient(new ServerAddress("173.212.77.18", 27017));
 
+        DB database = client .getDB("course");
+        DBCollection collection = database.getCollection("hello");
+
+        DBObject document = collection.findOne();
+        System.out.println(document);
     }
 }
